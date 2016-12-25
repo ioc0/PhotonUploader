eb7c46071bebaefa8570de23fffca2e99842434e
HOME-PC\Александр <ioc0kernel@gmail.com>
2016-12-25 22:13:43 +0300

    youtube

---
+++ PhotonUploader/PhotonUploader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PhotonUploader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhotonUploader")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c711497e-ef7c-46b0-b919-70987f62115e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PhotonUploader/PhotonUploader/Youtube.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.GData.Extensions;
+using Google.GData.YouTube;
+using Google.GData.Extensions.MediaRss;
+using Google.YouTube;
+using Google.GData.Client;
+
+namespace PhotonUploader
+{
+    class Youtube
+    {
+        public static string UploadVideo(string FilePath, string Title, string Description)
+        {
+            YouTubeRequestSettings settings;
+            YouTubeRequest request;
+            string devkey = "YOUR DEVELOPPER KEY HERE";
+            string username = "Your Youtube Username";
+            string password = "Your Youtube Password";
+            settings = new YouTubeRequestSettings("Your Application Name", devkey, username, password) { Timeout = -1 };
+            request = new YouTubeRequest(settings);
+
+            Video newVideo = new Video();
+
+            newVideo.Title = Title;
+            newVideo.Description = Description;
+            newVideo.Private = true;
+            newVideo.YouTubeEntry.Private = false;
+
+            //newVideo.Tags.Add(new MediaCategory("Autos", YouTubeNameTable.CategorySchema));
+
+            //newVideo.Tags.Add(new MediaCategory("mydevtag, anotherdevtag", YouTubeNameTable.DeveloperTagSchema));
+
+            //newVideo.YouTubeEntry.setYouTubeExtension("location", "Paris, FR");
+            // You can also specify just a descriptive string ==>
+            // newVideo.YouTubeEntry.Location = new GeoRssWhere(71, -111);
+            // newVideo.YouTubeEntry.setYouTubeExtension("location", "Paris, France.");
+
+            newVideo.YouTubeEntry.MediaSource = new MediaFileSource(FilePath, "video/mp4");
+            Video createdVideo = request.Upload(newVideo);
+
+            return createdVideo.VideoId;
+        }
+    }
+}
